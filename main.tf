e754d653d2ae02c957ee3ded15fafe465ca83652
MannDhakad <mdhakad1189@@gmail.com>
2026-04-02 13:22:31 +0530

    pipeline day02

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "azurerm" {}
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    features {}
+  
+}
+
+resource "azurerm_resource_group" "rg_pipeline02" {
+    name     = "rg-pipeline02"
+    location = "central india"
+  
+}
